d8ae95dfdf06adbc7b30491119f9669fea36b10f
ozi <ozisadiqkasai@gmail.com>
2023-04-05 07:57:57 +0100

    new file added

---
+++ MakeUpperCase.js
@@ -0,0 +1,12 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+
+    let string = str.toUpperCase()
+    return string
+
+     }
+let convert = makeUpperCase('hello')
+
+console.log(convert)
+
