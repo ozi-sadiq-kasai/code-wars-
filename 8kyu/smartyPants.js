fe973745410ec8af960fe5f402a4bff625853ef5
ozi <ozisadiqkasai@gmail.com>
2023-04-17 08:39:57 +0100

    new file

---
+++ 8kyu/smartyPants.js
@@ -0,0 +1,16 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    if((classPoints.reduce((a,b)=>a+b,0))/classPoints.length < yourPoints ){
+      return true
+    }
+    else{
+      return false
+    }
+  }
