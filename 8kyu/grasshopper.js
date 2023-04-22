4d4113fb35da0d5fd6071bef8d6f985672bd616a
ozi <ozisadiqkasai@gmail.com>
2023-04-22 21:16:31 +0100

    added file

---
+++ 8kyu/grasshopper.js
@@ -0,0 +1,29 @@
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+function getGrade (s1, s2, s3) {
+    let grade = (s1 + s2 + s3)/3
+    if(grade >= 90){
+        return 'A'
+    }
+    else if(grade >= 80 && grade < 90){
+        return 'B'
+    }
+    else if(grade >= 70 && grade < 80){
+        return 'C'
+    }
+    else if(grade >= 60 && grade < 70){
+        return 'D'
+    }
+    else if( grade < 60){
+        return 'F'
+    }
+
+  }
+
+  console.log(getGrade(84,79,85))
